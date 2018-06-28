8b70371bfa25a307726b351b87ddfc193dfed82d
Mit <mit_rupang@yahoo.ca>
2018-06-28 16:45:10 -0400

    script for rooms

--- update_rooms.py
+++ update_rooms.py
@@ -1,9 +1,30 @@
 """ Gets the rooms added to the room_images directory and update the JavaScript file."""
 from os import walk
 
-f = []
+rooms_dict = {}
 for (dirpath, dirnames, filenames) in walk('./images/room_images'):
-    f.extend(filenames)
-    print(dirpath, filenames)
+    if not('Before' in dirpath or 'After' in dirpath):
+        if 'completed' in dirpath.lower() or 'ongoing' in dirpath.lower() or 'pilot' in dirpath.lower():
+            type_dict = {}
+            for pic in filenames:
+                if '.jpg' in pic.lower():
+                    temp = pic.split('_')
+                    room_number = temp[0]
 
-print(f);
+                    temp2 = temp[1].split('-')
+                    season1 = temp2[0]
+                    season2 = temp2[1]
+                    year = temp2[2].split('.')[0]
+                    type_dict[room_number] = season1 + '/' + season2 + '-' + year
+
+            splitted_path = dirpath.split('/')
+            building = splitted_path[-2]
+            type = splitted_path[-1]
+            if building not in rooms_dict:
+                rooms_dict[building] = {}
+            rooms_dict[building][type] = type_dict
+
+with open('js/show_rooms.js', 'r+') as f:
+    content = f.read()
+    f.seek(0, 0)
+    f.write('var rooms_dict = ' + str(rooms_dict) + ';\n\n' + content)
