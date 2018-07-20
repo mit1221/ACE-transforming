f8cafdd9cdf591108d44e5674495267f6dac761d
Mit <mit_rupang@yahoo.ca>
2018-07-20 16:27:03 -0400

    working on scroll animation

--- TIL Website/js/animate_on_scroll.js
+++ TIL Website/js/animate_on_scroll.js
@@ -2,20 +2,17 @@ var className = '.callout';
 
 $(document).ready(function() {
   var elements = $(className);
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.opacity = 0;
-    elements[i].style.marginLeft = '-100px';
-  }
   animate_on_scroll();
   window.addEventListener('scroll', animate_on_scroll);
 
   var finished_animation = false;
   function animate_on_scroll() {
     if ((isInViewport(elements[0]) || isInViewport(elements[1])) && !finished_animation) {
-      $(className + ':eq(0)').animate({marginLeft: '0', opacity: 1}, 600);
-      setTimeout(function() {
-        $(className + ':eq(1)').animate({marginLeft: '0', opacity: 1}, 600);
-      }, 200);
+      // $(className + ':eq(0)').animate({marginLeft: '0', opacity: 1}, 600);
+      // setTimeout(function() {
+      //   $(className + ':eq(1)').animate({marginLeft: '0', opacity: 1}, 600);
+      // }, 100);
+      chain_animate('callout', 0.2);
       finished_animation = true;
       window.removeEventListener('scroll', animate_on_scroll);
     }
--- TIL Website/js/count_animation.js
+++ TIL Website/js/count_animation.js
@@ -3,27 +3,36 @@ var numbers;
 var data = [];
 
 $(document).ready(function() {
-  numbers_container = document.getElementsByClassName('numbers')[0];
-  numbers = document.getElementsByClassName('number');
-
-  for (var i = 0; i < numbers.length; i++) {
-    data.push(parseInt(numbers[i].dataset.number));
-    numbers[i].textContent = '-';
-  }
-  setTimeout(function() {
-    checkPosition();
-    window.addEventListener('scroll', checkPosition);
-  }, 1000);
+  // numbers_container = document.getElementsByClassName('numbers')[0];
+  // numbers = document.getElementsByClassName('number');
+  //
+  // for (var i = 0; i < numbers.length; i++) {
+  //   data.push(parseInt(numbers[i].dataset.number));
+  //   numbers[i].textContent = '-';
+  // }
+  // setTimeout(function() {
+  //   checkPosition();
+  //   window.addEventListener('scroll', checkPosition);
+  // }, 1000);
+  // for the 3 numbers on the landing page
+  var numbers = new AnimateOnScroll(
+                  document.getElementsByClassName('number'),
+                  function() {
+                    chain_animate('number-box', 0.2);
+                    // countUp();
+                  }
+                );
 });
 
-var finished = false;
-function checkPosition() {
-  if (isInViewport(numbers_container) && !finished) {
-    finished = true;
-    window.removeEventListener('scroll', checkPosition);
-    countUp();
-  }
-}
+// var finished = false;
+// function checkPosition() {
+//   if (isInViewport(numbers_container) && !finished) {
+//     finished = true;
+//     window.removeEventListener('scroll', checkPosition);
+//     chain_animate('number-box', 0.2);
+//     countUp();
+//   }
+// }
 
 
 function countUp() {
@@ -43,6 +52,56 @@ function countUp() {
     }
   }, 10);
 }
+
+
+
+function AnimateOnScroll(elements, animate_function) {
+  this.isFinished = [];
+  this.elements = elements;
+  this.el_length = this.elements.length;
+  this.animate_function = animate_function;
+  this.scroll_handler = function() {
+    for (var i = 0; i < this.el_length; i++) {
+      if (isInViewport(this.elements[i]) && !isFinished[i]) {
+        this.animate_function();
+        isFinished[i] = true;
+      }
+    }
+
+    // remove event listener if all the elements are animated
+    if (this.isFinished.every(function(el) {return el == true})) {
+      window.removeEventListener('scroll', this.scroll_handler);
+    }
+  };
+
+  /* set all elements in the isFinished array to false since none of the
+  animations are finished at initialization */
+  for (var i = 0; i < this.elements.length; i++) {
+    this.isFinished.push(false);
+  }
+
+  /* if the user has not scrolled yet and an animatable element is in view,
+  the animation should happen */
+  this.scroll_handler();
+
+  this.start_listener();
+}
+
+AnimateOnScroll.prototype.start_listener = function() {
+  window.addEventListener('scroll', this.scroll_handler);
+}
+
+
+
+// // for the comments on the landing landing_page_container
+// var callout = new AnimateOnScroll()
+//
+
+
+
+
+
+
 
 // Determine if an element is in the visible viewport
 function isInViewport(element) {
