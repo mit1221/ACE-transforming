d4e2d5588f301442d93cabb199039b915e85c34c
Mit <mit_rupang@yahoo.ca>
2018-07-23 16:43:15 -0400

    done scrolling animations code

--- TIL Website/js/animate_on_scroll.js
+++ TIL Website/js/animate_on_scroll.js
@@ -1,20 +1,99 @@
-var className = '.callout';
+// making the 3 numbers count up
+function countUp(element) {
+  var numberElement = element.getElementsByClassName('number')[0];
+  var number = parseInt(numberElement.dataset.number);
+  var j = 0;
 
-$(document).ready(function() {
-  var elements = $(className);
-  animate_on_scroll();
-  window.addEventListener('scroll', animate_on_scroll);
+  var timer = setInterval(function() {
+    if (j > 69) {
+      clearInterval(timer);
+      numberElement.textContent = number;
+    } else {
+      numberElement.textContent =  Math.ceil(j * (number / 70));
+      j++;
+    }
+  }, 10);
+}
 
-  var finished_animation = false;
-  function animate_on_scroll() {
-    if ((isInViewport(elements[0]) || isInViewport(elements[1])) && !finished_animation) {
-      // $(className + ':eq(0)').animate({marginLeft: '0', opacity: 1}, 600);
-      // setTimeout(function() {
-      //   $(className + ':eq(1)').animate({marginLeft: '0', opacity: 1}, 600);
-      // }, 100);
-      chain_animate('callout', 0.2);
-      finished_animation = true;
-      window.removeEventListener('scroll', animate_on_scroll);
+
+
+function AnimateOnScroll(elements, gap, animation, extraAnimations) {
+  this.isFinished = [];
+  this.elements = (HTMLCollection.prototype.isPrototypeOf(elements) ||
+                  NodeList.prototype.isPrototypeOf(elements)) ?
+                  elements : [elements];
+
+  // make all elements invisible at the start
+  for (var i = 0; i < this.elements.length; i++) {
+    this.elements[i].style.opacity = 0;
+  }
+
+  this.gap = gap;
+  this.animateFunction = function(el) {
+    el.style.WebkitAnimation = animation + ' forwards';
+    el.style.animation = animation + ' forwards';
+
+    // call other animations, if any
+    if (extraAnimations !== undefined) {
+      for (var i = 0; i < extraAnimations.length; i++) {
+        extraAnimations[i](el);
+      }
+    }
+  };
+  this.scrollHandlerBinded = this.scrollHandler.bind(this);
+
+  /* set all elements in the isFinished array to false since none of the
+  animations are finished at initialization */
+  for (var i = 0; i < this.elements.length; i++) {
+    this.isFinished.push(false);
+  }
+
+  this.start();
+}
+
+AnimateOnScroll.prototype.start = function() {
+  /* if the user has not scrolled yet and an animatable element is in view,
+  the animation should happen */
+  this.scrollHandler();
+  window.addEventListener('scroll', this.scrollHandlerBinded);
+}
+
+AnimateOnScroll.prototype.scrollHandler = function() {
+  // whenever any of the elements come in the viewport, animate them in
+  var queue = [];
+  for (var counter = 0; counter < this.elements.length; counter++) {
+    if (isInViewport(this.elements[counter]) && !this.isFinished[counter]) {
+      this.isFinished[counter] = true;
+      queue.push(this.elements[counter]);
     }
   }
-});
+
+  /* animate each element in the queue one by one until queue is empty
+  if multiple elements are in the view */
+  var counter = 0;
+  while (queue.length > 0) {
+    doSetTimeout(counter, queue.shift(), this);
+    counter++;
+  }
+
+  // setting the gap between each element's animation
+  function doSetTimeout(i, el, object) {
+    setTimeout(function() {
+      object.animateFunction(el);
+    }, object.gap * i);
+  }
+
+  // remove event listener if all the elements are animated
+  if (this.isFinished.every(function(val) {return val == true})) {
+    window.removeEventListener('scroll', this.scrollHandlerBinded);
+  }
+}
+
+
+// Determine if an element is in the visible viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+  return rect.top > -(element.clientHeight) + (height / 3) && rect.top < height - (height / 3);
+}
--- TIL Website/js/chain_animation.js
+++ TIL Website/js/chain_animation.js
@@ -1,8 +1,7 @@
-function chain_animate(className, gap) {
+function chainAnimate(className, gap) {
   var elements = document.getElementsByClassName(className);
   var delay = 0;
   for (var i = 0; i < elements.length; i++) {
-    elements[i].style.animationPlayState = 'running';
     elements[i].style.animationDelay = delay + 's';
     delay += gap;
   }
