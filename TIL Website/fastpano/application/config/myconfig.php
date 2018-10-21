524adeb2d6fe6051f236303d9df792742e4936e4
Mit <mit_rupang@yahoo.ca>
2018-10-20 22:40:36 -0400

    changes

--- TIL Website/fastpano/application/config/myconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['myconfig_username'] = '360photos';
-$config['myconfig_password'] = '360photos';
-$config['myconfig_upload_path'] = 'upload';
-$config['myconfig_virtualtour_path'] = 'vtours';
-
-?>
