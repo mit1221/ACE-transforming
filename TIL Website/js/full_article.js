2a0b1f1d02502ccd2b1c10a35f14406949355cad
Mit <mit_rupang@yahoo.ca>
2018-10-30 10:59:56 -0400

    latest news section added

--- TIL Website/js/full_article.js
+++ TIL Website/js/full_article.js
@@ -1,27 +1,35 @@
 $(document).ready(displayInfo);
 
 function displayInfo() {
-  var articleInfo = newsInfo[getPostId()];
+  var articleInfo = null;
+  for (var i = 0; i < newsInfo.length; i++) {
+    if (newsInfo[i].title == getPostTitle()) {
+      articleInfo = newsInfo[i];
+      break;
+    }
+  }
+
   var title = articleInfo.title;
   var author = articleInfo.author;
   var date = articleInfo.date;
-  var content = articleInfo.content;
+  var contentHTML = articleInfo.content;
   var image = articleInfo.image;
+
+  document.title = title;
 
   var mainTitle = '<h1 style="margin-top: 0">' + title + '</h1>';
   var dateAndAuthor = '<h6 style="margin-top: 30px; margin-bottom: 5px;">By <span class="name">' + author + '</span> <span style="color: #CCC">|</span> <span class="date">' +
   date + '</span></h6>';
   var imageHTML = image != undefined ? '<img src="images/news_images/' + image + '"/>' : '';
-  var contentHTML = formatText(content);
 
   var articleHTML = dateAndAuthor + mainTitle + '<div class="bar"></div>' + imageHTML + contentHTML;
   $('#full-article').html(articleHTML);
 }
 
-function getPostId() {
-  return window.location.hash.substr(1);
+function getPostTitle() {
+  return decodeURL(window.location.hash.substr(1));
 }
 
-function formatText(text) {
-  return '<p>' + text + '</p>';
+function decodeURL(url) {
+  return decodeURIComponent(url);
 }
--- TIL Website/js/news.js
+++ TIL Website/js/news.js
@@ -1,13 +1,49 @@
-$(document).ready(generatePosts);
+$(document).ready(populatePage);
 
 const POST_LENGTH = 300;
+const LATEST_POSTS_TO_DISPLAY = 3;
+var newsArray = [];
+
+function populatePage() {
+  for (var i = 0; i < newsInfo.length; i++) {
+    newsArray.push(newsInfo[i]);
+  }
+
+  // sort articles by date
+  newsArray.sort(function(a,b){
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
+    return new Date(b.date) - new Date(a.date);
+  });
+
+  populateLatestNews();
+  generatePosts();
+}
+
+function populateLatestNews() {
+  var latestNewsHTML = '';
+
+  for (var i = 0; i < LATEST_POSTS_TO_DISPLAY; i++) {
+    var article = newsArray[i];
+    var dateHTML = '<h6 style="margin-top: 30px; margin-bottom: 5px;"><span class="date">' +
+    article.date + '</span></h6>';
+
+    latestNewsHTML += '<div class="latest-news-item">' + dateHTML + '<a href="full_article.html#' + encodeURL(article.title) + '">'
+    + '<h4 class="latest-news-text">' + article.title + '</h4>' + '</a></div>';
+  }
+
+  $('.latest-news-content').html(latestNewsHTML);
+}
+
 
 function generatePosts() {
   var articleHTML = '';
 
   // generate a post for each news item
-  for (id in newsInfo) {
-    var articleInfo = newsInfo[id];
+  for (var i = 0; i < newsArray.length; i++) {
+    var articleInfo = newsArray[i];
+
+    var id = articleInfo.id;
     var title = articleInfo.title;
     var author = articleInfo.author;
     var date = articleInfo.date;
@@ -17,7 +53,7 @@ function generatePosts() {
     '<div class="post"><h3>' + title + '</h3>' +
     '<h6>By <span class="name">' + author + ' </span><span style="color: #CCC">|</span> <span class="date">' +
     date + '</span></h6><p>' + reduceNewsText(content) + '</p>' +
-    '<a href="full_article.html#' + id + '" class="link" style="font-size: 0.9em">Continue reading &#187;</a></div>';
+    '<a href="full_article.html#' + encodeURL(title) + '" class="link" style="font-size: 0.9em">Continue reading &#187;</a></div>';
   }
   $('.news').html(articleHTML);
 
@@ -31,4 +67,9 @@ function reduceNewsText(text) {
     var temp = originalText.substring(0, POST_LENGTH + 1);
     return temp.substring(0, temp.lastIndexOf(" ")) + " ...";
   }
+}
+
+// makes text URL friendly
+function encodeURL(url) {
+  return encodeURIComponent(url);
 }
