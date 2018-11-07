75bc9f8a761192a59c4bc58316a5bf31c36ee409
Mit <mit_rupang@yahoo.ca>
2018-11-07 15:02:24 -0500

    changes

--- TIL Website/js/news_data.js
+++ TIL Website/js/news_data.js
@@ -10,22 +10,21 @@ var newsInfo = [{
       "<p>We are currently looking for students to <strong>participate in 1-hour interviews</strong> as part of this research. If you are interested in participating in this study, or would like more information, please contact <a href='mailto:shirin.gerami@mail.utoronto.ca'>shirin.gerami@mail.utoronto.ca</a>. </p>" +
       "<p>The insights from this research project will be shared with the Family Care Office, aiding their efforts to better serve and support students with family care-giving responsibilities. It is designed to give voice to your unique experiences as a student with family-care giving responsibilities, as well as to use the insights gathered to identify resources and support systems needed to enrich your experience at University of Toronto. We want to make sure that your voice is heard and that your experience is accounted for!</p>"
   },
-{
-        'title': 'How to Better Support UofT Students: The Future of SLP Event!',
-        'author': 'By Suzie Kim, Communications Team',
-        'date': 'November 2, 2018 ',
-        'image': '181102.jpg',
-        'link': 'http://blogs.studentlife.utoronto.ca/innovationhub/how-to-better-support-uoft-students-the-future-of-slp-event/#more-2437',
-        'summary': 
-"On October 11th, 2018, the Innovation Hub partnered with the SLP network to host a research event entitled SLP Presents: The Future of SLP.  SLP is a network of staff who  work directly with students across at the University of Toronto’s three campuses.  The partnership between SLP and the Innovation Hub commenced last year to explore two questions: What should the future of the SLP Network look like? and how should this community of practice be redesigned to better serve the needs and wants of all its members? ",
-        'content': "<P>On October 11th, 2018, the Innovation Hub partnered with the SLP network to host a research event entitled <strong>SLP Presents: The Future of SLP</strong>.  SLP is a network of staff who  work directly with students across at the University of Toronto’s three campuses.  The partnership between SLP and the Innovation Hub commenced last year to explore two questions: What should the future of the SLP Network look like? and how should this community of practice be redesigned to better serve the needs and wants of all its members?  After conducting a number of empathy interviews with staff in a variety of positions and career stages, insights were generated.  These insights reveled that supporting staff to develop institutional knowledge, mentorship and networking, senior management involvement and career development support are areas where there are opportunities for SLP to provide support.  This event sought to gather additional data to inform next steps of the process.<p/>" +
-"<img src='images/news_images/181107b.jpg'/>" +
-"<p>The event featured an inclusive and welcoming environment, where colleagues with similar motives, skills, and responsibilities came together to share ideas and struggles that appear in their work on campus.</p>" +
-"<p>After an opening introduction and a brief presentation of past research insights, the event focused on encouraging participants to engage in activities and discussion. Various booths around the room focused on different topics, like career path, professional learning, connections, community and supporting students.</p>" +
-"<img src='images/news_images/181102c.jpg'/>" +
-"<p>Our research team is now working with the data and artifacts collected at this event. We will analyze and synthesize the data and insights for the future of the SLP Network. Stay tuned for our results, to be posted on this blog in the coming months!</p>" +
-"<p>Note: a copy of the Innovation Hub’s SLP research report is available to UofT staff by request at<a href='innovationhub@utoronto.ca'></a></p>" 
-    },
+  {
+    'title': 'How to Better Support UofT Students: The Future of SLP Event!',
+    'author': 'By Suzie Kim, Communications Team',
+    'date': 'November 2, 2018 ',
+    'image': '181102.jpg',
+    'link': 'http://blogs.studentlife.utoronto.ca/innovationhub/how-to-better-support-uoft-students-the-future-of-slp-event/#more-2437',
+    'summary': "On October 11th, 2018, the Innovation Hub partnered with the SLP network to host a research event entitled SLP Presents: The Future of SLP.  SLP is a network of staff who  work directly with students across at the University of Torontoï¿½s three campuses.  The partnership between SLP and the Innovation Hub commenced last year to explore two questions: What should the future of the SLP Network look like? and how should this community of practice be redesigned to better serve the needs and wants of all its members? ",
+    'content': "<P>On October 11th, 2018, the Innovation Hub partnered with the SLP network to host a research event entitled <strong>SLP Presents: The Future of SLP</strong>.  SLP is a network of staff who  work directly with students across at the University of Torontoï¿½s three campuses.  The partnership between SLP and the Innovation Hub commenced last year to explore two questions: What should the future of the SLP Network look like? and how should this community of practice be redesigned to better serve the needs and wants of all its members?  After conducting a number of empathy interviews with staff in a variety of positions and career stages, insights were generated.  These insights reveled that supporting staff to develop institutional knowledge, mentorship and networking, senior management involvement and career development support are areas where there are opportunities for SLP to provide support.  This event sought to gather additional data to inform next steps of the process.<p/>" +
+      "<img src='images/news_images/181107b.jpg'/>" +
+      "<p>The event featured an inclusive and welcoming environment, where colleagues with similar motives, skills, and responsibilities came together to share ideas and struggles that appear in their work on campus.</p>" +
+      "<p>After an opening introduction and a brief presentation of past research insights, the event focused on encouraging participants to engage in activities and discussion. Various booths around the room focused on different topics, like career path, professional learning, connections, community and supporting students.</p>" +
+      "<img src='images/news_images/181102c.jpg'/>" +
+      "<p>Our research team is now working with the data and artifacts collected at this event. We will analyze and synthesize the data and insights for the future of the SLP Network. Stay tuned for our results, to be posted on this blog in the coming months!</p>" +
+      "<p>Note: a copy of the Innovation Hubï¿½s SLP research report is available to UofT staff by request at<a href='innovationhub@utoronto.ca'></a></p>"
+  },
 
   /*
     {
